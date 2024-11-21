17b6b1b979982c6b5a8e3e5bcd13c6ba5d0ed83e
3Csabrina <00001121604511sp@al.educacao.sp.gov.br>
2024-11-21 11:34:55 -0300

    Add files via upload

---
+++ Ferramenta de estudo Sabrina F3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é vscode?',
+    'O vscode é um repositorio de programação'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a diferença de circulo e circunferência?',
+    'Circulo é o preenchimento da circunferência e circunferência e o contorno'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "eu te amo" em Inglês?',
+    '"eu te amo" em ingles é "i love you" (ai lovi iu)'
+)
